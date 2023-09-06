534917a0c0c11c1d2eb8533644861875bf65ab08
HolyCoder13 <adrian.madejski@vp.pl>
2023-09-06 18:46:49 +0200

    rest with h2 database

--- Spring2023/ToDoApp/todo-app/src/main/java/io/gitub/mat3e/model/TaskRepository.java
+++ Spring2023/ToDoApp/todo-app/src/main/java/io/gitub/mat3e/model/TaskRepository.java
@@ -1,17 +1,21 @@
 package io.gitub.mat3e.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.repository.query.Param;
 
-@RepositoryRestResource(path = "list", collectionResourceRel = "to dos")
-interface TaskRepository extends JpaRepository<Task,Integer> {
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+import java.util.Optional;
+public interface TaskRepository {
+    List<Task> findAll();
+    Page<Task> findAll(Pageable page);
+    Optional<Task> findById(Integer i);
 
-    @Override
-    @RestResource(exported = false)
-    void deleteById(Integer integer);
+    boolean existsById(Integer id);
+    Task save(Task entity);
+    List<Task> findByDone(@Param("state") boolean done);
 
-    @Override
-    @RestResource(exported = false)
-    void delete(Task entity);
+
+   // Page<Task> findAll(org.springframework.data.domain.Pageable page);
 }

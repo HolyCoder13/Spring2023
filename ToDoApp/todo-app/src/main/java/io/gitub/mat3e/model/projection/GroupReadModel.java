749738d9b3264fb23ff238d6bdb850bf8f7d8104
HolyCoder13 <adrian.madejski@vp.pl>
2023-10-09 12:49:02 +0200

    unit test of GroupReadModel, exception null deadLine handling in DTO

--- ToDoApp/todo-app/src/main/java/io/gitub/mat3e/model/projection/GroupReadModel.java
+++ ToDoApp/todo-app/src/main/java/io/gitub/mat3e/model/projection/GroupReadModel.java
@@ -5,6 +5,7 @@ import io.gitub.mat3e.model.TaskGroup;
 
 import java.time.LocalDateTime;
 import java.util.Comparator;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -20,6 +21,7 @@ public class GroupReadModel {
         description= source.getDescription();
         source.getTasks().stream()
                 .map(Task::getDeadline)
+                .filter(Objects::nonNull)
                 .max(LocalDateTime::compareTo)
                 .ifPresent(date -> deadLine = date);
         tasks = source.getTasks().stream()

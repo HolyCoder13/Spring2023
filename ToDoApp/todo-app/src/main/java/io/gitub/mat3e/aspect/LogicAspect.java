467d3203448f6dcbe34019cce0f995bfed824238
HolyCoder13 <adrian.madejski@vp.pl>
2023-10-23 12:02:04 +0200

    project working fine, before group operations fix

--- ToDoApp/todo-app/src/main/java/io/gitub/mat3e/aspect/LogicAspect.java
+++ ToDoApp/todo-app/src/main/java/io/gitub/mat3e/aspect/LogicAspect.java
@@ -2,9 +2,12 @@ package io.gitub.mat3e.aspect;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,7 +20,14 @@ public class LogicAspect {
     LogicAspect(final MeterRegistry registry){
         projectCreateGroupTimer = registry.timer("logic.project.create.group");
     }
-    @Around("execution(* io.gitub.mat3e.logic.ProjectService.createGroup(..))")
+    @Pointcut("execution (* io.gitub.mat3e.logic.ProjectService.createGroup(..))")
+    static void projectServiceCreateGroup(){
+    }
+    @Before("projectServiceCreateGroup()")
+    void logMethodCall(JoinPoint jp){
+        logger.info("Before {} with {}",jp.getSignature().getName());
+    }
+    @Around("projectServiceCreateGroup()")
     Object aroundProjectCreateGroup(ProceedingJoinPoint jp){
         return projectCreateGroupTimer.record(()->{
             try{

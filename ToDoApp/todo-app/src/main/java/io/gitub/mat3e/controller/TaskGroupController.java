06da90f2943ee832a9eafb33212a52e994f77f5c
HolyCoder13 <adrian.madejski@vp.pl>
2023-10-23 12:05:01 +0200

    taskGroupController update, lgfm

--- ToDoApp/todo-app/src/main/java/io/gitub/mat3e/controller/TaskGroupController.java
+++ ToDoApp/todo-app/src/main/java/io/gitub/mat3e/controller/TaskGroupController.java
@@ -1,96 +1,112 @@
 package io.gitub.mat3e.controller;
 
 import io.gitub.mat3e.logic.TaskGroupService;
-import io.gitub.mat3e.model.*;
+import io.gitub.mat3e.model.Task;
+import io.gitub.mat3e.model.TaskRepository;
 import io.gitub.mat3e.model.projection.GroupReadModel;
 import io.gitub.mat3e.model.projection.GroupTaskWriteModel;
 import io.gitub.mat3e.model.projection.GroupWriteModel;
-import io.gitub.mat3e.model.projection.ProjectWriteModel;
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 
 @Controller
-@IllegalExceptionProcessing
 @RequestMapping("/groups")
-public class TaskGroupController {
-    private final Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
-    private final TaskRepository repository;
+class TaskGroupController {
+    private static final Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
     private final TaskGroupService service;
+    private final TaskRepository repository;
 
-    public TaskGroupController(TaskRepository repository, TaskGroupService service) {
-        this.repository = repository;
+    TaskGroupController(final TaskGroupService service, final TaskRepository repository) {
         this.service = service;
+        this.repository = repository;
     }
 
     @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
-    String showGroups(Model model){
-        model.addAttribute("group",new GroupWriteModel());
+    String showGroups(Model model) {
+        model.addAttribute("group", new GroupWriteModel());
         return "groups";
     }
+
     @PostMapping(produces = MediaType.TEXT_HTML_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     String addGroup(
-                    @ModelAttribute("group") @Valid GroupWriteModel current,
-                    BindingResult bindingResult,
-                    Model model
-            ){
-        if(bindingResult.hasErrors()){
+            @ModelAttribute("group") @Valid GroupWriteModel current,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
             return "groups";
         }
         service.createGroup(current);
         model.addAttribute("group", new GroupWriteModel());
-        model.addAttribute("group", getGroups());
-        model.addAttribute("message", "Group added!");
+        model.addAttribute("groups", getGroups());
+        model.addAttribute("message", "Dodano grupę!");
         return "groups";
     }
+
     @PostMapping(params = "addTask", produces = MediaType.TEXT_HTML_VALUE)
-    String addGroupTask(@ModelAttribute("group") GroupWriteModel current){
+    String addGroupTask(@ModelAttribute("group") GroupWriteModel current) {
         current.getTasks().add(new GroupTaskWriteModel());
         return "groups";
     }
+
     @ResponseBody
-    @PostMapping(produces = MediaType.APPLICATION_FORM_URLENCODED_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<GroupReadModel> createGroup(@RequestBody @Valid GroupWriteModel toCreate){
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<GroupReadModel> createGroup(@RequestBody @Valid GroupWriteModel toCreate) {
         GroupReadModel result = service.createGroup(toCreate);
-        return ResponseEntity.created(URI.create("/"+result.getId())).body(result);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
     @ResponseBody
-    @GetMapping(produces= MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<List<GroupReadModel>> readAllGroups(){
-        logger.info("Exposing taskGroups");
-        return  ResponseEntity.ok(service.readAll());
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<List<GroupReadModel>> readAllGroups() {
+        return ResponseEntity.ok(service.readAll());
     }
+
     @ResponseBody
-    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<List<Task>> readAllTasksFromGroup(@PathVariable int id){
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<List<Task>> readAllTasksFromGroup(@PathVariable int id) {
         return ResponseEntity.ok(repository.findAllByGroup_Id(id));
     }
+
     @ResponseBody
     @Transactional
     @PatchMapping("/{id}")
-    public ResponseEntity<?> toggleGroup(@PathVariable int id){
+    public ResponseEntity<?> toggleGroup(@PathVariable int id) {
         service.toggleGroup(id);
-               return ResponseEntity.noContent().build() ;
+        return ResponseEntity.noContent().build();
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    ResponseEntity<?> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    ResponseEntity<String> handleIllegalState(IllegalStateException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 
     @ModelAttribute("groups")
-    public List<GroupReadModel> getGroups() {
+    List<GroupReadModel> getGroups() {
         return service.readAll();
     }
-
 }
